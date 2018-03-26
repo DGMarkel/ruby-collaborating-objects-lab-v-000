b82beae46834d931fa30f440eb46b435687e4c9c
David Markel <davidgmarkel@gmail.com>
2018-03-26 23:21:10 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @songs
+  end
+
+  def save
+    Song.all << self
+  end
+
+  def songs
+    @songs
+
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    if new_artist = Song.all.find{|artist| artist.name == name}
+      new_artist
+    else
+      new_artist = Artist.new(name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+
+end
